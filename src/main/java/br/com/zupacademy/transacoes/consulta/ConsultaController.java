05e9e1c512a6e8d4912acf754610c7dfd30f7361
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-09-29 18:35:17 -0300

    orElseThrow na busca pelo cartão

--- src/main/java/br/com/zupacademy/transacoes/consulta/ConsultaController.java
+++ src/main/java/br/com/zupacademy/transacoes/consulta/ConsultaController.java
@@ -5,13 +5,16 @@ import br.com.zupacademy.transacoes.model.Transacao;
 import br.com.zupacademy.transacoes.repository.CartaoRepository;
 import br.com.zupacademy.transacoes.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/compras")
@@ -26,7 +29,7 @@ public class ConsultaController {
     @GetMapping("/{idCartao}")
     public ResponseEntity<?> busca(@PathVariable Long idCartao) {
 
-        Cartao cartao = cartaoRepository.findById(idCartao).orElse(null);
+        Cartao cartao = cartaoRepository.findById(idCartao).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cartão não existente!"));
 
         List<Transacao> transacoes = transacaoRepository.findAllByCartao(cartao);
 
