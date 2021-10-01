46317e91abfe1d7ea58af9dc3e27a774afe5a49f
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-10-01 10:19:09 -0300

    adicionando dependência oauth2 e resolvendo exception security

--- src/main/java/br/com/zupacademy/transacoes/seguranca/SecurityConfiguration.java
+++ src/main/java/br/com/zupacademy/transacoes/seguranca/SecurityConfiguration.java
@@ -2,10 +2,12 @@ package br.com.zupacademy.transacoes.seguranca;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -16,7 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/cartoes/**/transacoes")
+                .antMatchers("/cartoes/*/transacoes")
                 .hasAuthority("SCOPE_read")
                 .anyRequest().authenticated()
                 .and()
